4c8b3df4432c17a32478385cc55ae4495cb73d69
James Garland <james@jamesgarland.net>
2020-02-08 21:51:04 -0500

    Updated double slit calculator

--- double_slit.py
+++ double_slit.py
@@ -1,31 +1,35 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
+#test values
+#l = 700 * (10**(-9))
+#d = 20 * (10**(-6))
+#a = 5 * (10**(-6))
+
 #init values
 l = float(input("Wavelength (nm): ")) * (10**(-9))
-#D = 10
 d = float(input("Distance between slits (um): ")) * (10**(-6))
 a = float(input("Slit width (um): ")) * (10**(-6))
-I0 = 1
+I0 = float(input("Intensity: "))
+
 #create empty value array
-IVals = np.zeros([1440])
+IVals = np.zeros([1920])
 
-#def intensity(t):
-#    I = 4 * I0 * ((np.sin(np.pi * a/l * np.sin(t))/(np.pi * a/l * np.sin(t)))**2) * (np.cos(np.pi * d/l * np.sin(t)))**2
-#    return I
+#calculate intensity (step of 1/16)
+for theta in range(-960, 960):
+    #convert to radians and scale from 1920 -> 60
+    t = np.radians(theta/16)
+    I = I0 * ((np.sin(np.pi * a/l * np.sin(t))/(np.pi * a/l * np.sin(t)))**2) * ((np.cos(np.pi * d/l * np.sin(t)))**2)
+    #place points in correct index
+    IVals[theta-960] = I
 
-#calculate intensity (step of 0.25)
-for theta in range(1440):
-    #convert to radians and scale from 1440 -> 360
-    t = np.radians(theta/4)
-    print(theta, t)
-    I = 4 * I0 * ((np.sin(np.pi * a/l * np.sin(t))/(np.pi * a/l * np.sin(t)))**2) * ((np.cos(np.pi * d/l * np.sin(t)))**2)
-    #print(I)
-    IVals[theta] = I
-
-#print(IVals)
+#plot setup
+plt.margins(x=0.001) #this is weird
+plt.xlabel(r"$\theta$")
+plt.ylabel("Intensity")
+plt.xticks(np.arange(-90, 90, 15))
+xRange = np.arange(-60, 60, 1/16)
 
 #plot intensity vs theta
-plt.xlim(360,1080)
-plt.plot(IVals)
+plt.plot(xRange, IVals)
 plt.show()
